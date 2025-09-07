af5c893270768bf491e824f8ccbfabac58622ed0
Rajesh Malviya <01rajeshnmalviya@gmail.com>
2025-09-07 10:43:48 +0000

    Message- Remvove the bold

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -85,6 +85,7 @@ const ChatPage: React.FC = () => {
     setCurrentMessageId,
     regenerate,
     continueGenerate,
+    getPostedModel,
     loadingConversation,
     getShouldContinue,
     relatedDocuments,
@@ -548,6 +549,11 @@ const ChatPage: React.FC = () => {
               </div>
             )}
           </div>
+          {getPostedModel() && (
+            <div className="absolute right-2 top-10 text-xs text-dark-gray dark:text-light-gray">
+              model: {getPostedModel()}
+            </div>
+          )}
         </div>
         <section className="relative size-full flex-1 overflow-auto pb-9">
           <div className="h-full">

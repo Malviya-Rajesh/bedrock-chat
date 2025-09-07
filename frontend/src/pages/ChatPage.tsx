91071273ebfd1606e5e4f09e8a5eeafae984ca76
Rajesh Malviya <01rajeshnmalviya@gmail.com>
2025-09-07 09:58:13 +0000

    Model NAme from Right hand side top Removed.

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -85,7 +85,6 @@ const ChatPage: React.FC = () => {
     setCurrentMessageId,
     regenerate,
     continueGenerate,
-    getPostedModel,
     loadingConversation,
     getShouldContinue,
     relatedDocuments,
@@ -549,11 +548,6 @@ const ChatPage: React.FC = () => {
               </div>
             )}
           </div>
-          {getPostedModel() && (
-            <div className="absolute right-2 top-10 text-xs text-dark-gray dark:text-light-gray">
-              model: {getPostedModel()}
-            </div>
-          )}
         </div>
         <section className="relative size-full flex-1 overflow-auto pb-9">
           <div className="h-full">
